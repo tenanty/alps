a2e05568cdb1013eb7499adf948567be56805b4f
yuanshun <tenanty@gmail.com>
2015-01-07 17:09:34 +0800

    增加欢迎界面 *欢迎页面内能实现切换 *增加预期的菜单选项 *使用requestWindowFeature(Window.FEATURE_NO_TITLE)去除活动中的标题栏 *增加聊天背景条

--- app/src/main/java/com/channelsoft/alps/activity/Activity4.java
+++ app/src/main/java/com/channelsoft/alps/activity/Activity4.java
@@ -2,6 +2,7 @@ package com.channelsoft.alps.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.channelsoft.alps.R;
 import com.channelsoft.alps.activity.base.BaseActivity;
@@ -10,11 +11,17 @@ import com.channelsoft.alps.activity.base.BaseActivity;
  * 设置页面效果
  */
 public class Activity4 extends BaseActivity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.tab4_layout);
-	}
+    private Button button = null;
+    private ImageView
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab4_layout);
+
+
+    }
 
 }
