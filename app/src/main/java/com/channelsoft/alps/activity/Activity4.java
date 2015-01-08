5ae411523afa70c34063dd0c02fc12581014232a
yuanshun <tenanty@gmail.com>
2015-01-08 18:23:24 +0800

    增加欢迎界面 *欢迎页面内能实现切换 *增加预期的菜单选项 *使用requestWindowFeature(Window.FEATURE_NO_TITLE)去除活动中的标题栏 *增加聊天背景条 *完善页面信息

--- app/src/main/java/com/channelsoft/alps/activity/Activity4.java
+++ app/src/main/java/com/channelsoft/alps/activity/Activity4.java
@@ -1,19 +1,40 @@
 package com.channelsoft.alps.activity;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.channelsoft.alps.R;
 import com.channelsoft.alps.activity.base.BaseActivity;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * 设置页面效果
  */
 public class Activity4 extends BaseActivity {
-    private Button button = null;
-    private ImageView
 
+    public static final int TAKE_PHOTO = 1;
+    public static final int CROP_PHOTO = 2;
+
+
+    private Button takePhoto = null;
+    private ImageView picture = null;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +42,91 @@ public class Activity4 extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tab4_layout);
 
+//        takePhoto = (Button) findViewById(R.id.take_photo);
+        picture = (ImageView) findViewById(R.id.picture);
+//        takePhoto.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                File outputImage = new File(Environment.getExternalStorageDirectory(), "output_image.jpg");
+//
+//                //判断文件是否存在,如果存在则删除文件
+//                if (outputImage.exists()) {
+//                    outputImage.delete();
+//                }
+//
+//                imageUri = Uri.fromFile(outputImage);
+//                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");//
+//                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+//                startActivityForResult(intent, TAKE_PHOTO);//启动相机程序
+//
+//            }
+//        });
+
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    Intent intent = new Intent("com.android.camera.action.CROP");
+                    intent.setDataAndType(imageUri, "image/*");
+                    intent.putExtra("scale", true);
+
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                    startActivityForResult(intent, CROP_PHOTO);//启动剪裁程序
+                }
+                break;
+            case CROP_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    Bitmap bitmap = null;
+                    try {
+                        bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        bitmap = getRoundedCornerBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    picture.setImageBitmap(bitmap);//将剪裁后的照片显示出来
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
+
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final Paint paint = new Paint();
+        //保证是方形，并且从中心画
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        int w;
+        int deltaX = 0;
+        int deltaY = 0;
+        if (width <= height) {
+            w = width;
+            deltaY = height - w;
+        } else {
+            w = height;
+            deltaX = width - w;
+        }
+        final Rect rect = new Rect(deltaX, deltaY, w, w);
+        final RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        //圆形，所有只用一个
+
+        int radius = (int) (Math.sqrt(w * w * 2.0d) / 2);
+        canvas.drawRoundRect(rectF, radius, radius, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
 }
