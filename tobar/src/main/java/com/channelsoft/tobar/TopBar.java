18d9e049e12621627d8f68b024ed375c1f9a7799
yuanshun <tenanty@gmail.com>
2015-01-12 09:42:22 +0800

    增加欢迎界面 *欢迎页面内能实现切换 *增加预期的菜单选项 *使用requestWindowFeature(Window.FEATURE_NO_TITLE)去除活动中的标题栏 *增加聊天背景条 *完善页面信息 提交tobar组件

---
+++ tobar/src/main/java/com/channelsoft/tobar/TopBar.java
@@ -0,0 +1,20 @@
+package com.channelsoft.tobar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * 自定义topBar
+ * Created by yuanshun on 2015/1/9.
+ */
+public class TopBar extends LinearLayout {
+
+    public TopBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+
+
+    }
+
+}
