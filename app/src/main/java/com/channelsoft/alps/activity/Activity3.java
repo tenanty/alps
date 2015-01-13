4a452a06d6390aac2988d83ebedcc12d96e2e5f9
yuanshun <tenanty@gmail.com>
2015-01-13 16:34:46 +0800

    topbar模块更新 增加topbar组件，并更新到相关页面中

--- app/src/main/java/com/channelsoft/alps/activity/Activity3.java
+++ app/src/main/java/com/channelsoft/alps/activity/Activity3.java
@@ -3,15 +3,25 @@ package com.channelsoft.alps.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 import com.channelsoft.alps.R;
 import com.channelsoft.alps.activity.base.BaseActivity;
+import com.channelsoft.alps.activity.module.Topbar;
 
+/**
+ * 第三个页面,显示好友信息
+ */
 public class Activity3 extends BaseActivity {
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-	// TODO Auto-generated method stub
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.tab3_layout);
-}
+    Topbar rl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab3_layout);
+
+        rl = (Topbar) findViewById(R.id.topbar);
+
+    }
 }
---
+++ topbar/src/main/java/channelsoft/com/myapplication/Topbar.java
@@ -0,0 +1,110 @@
+package channelsoft.com.myapplication;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created by yuanshun on 2015/1/13.
+ */
+public class Topbar extends RelativeLayout {
+
+    //基本元素信息：两个按钮+一个描述
+    private Button leftButton, rightButton;
+    private TextView tvTitle;
+
+    //左按钮的修饰信息
+    private int leftTextColor;
+    private Drawable leftBackground;
+    private String leftText;
+
+    //右按钮的修饰信息
+    private int rightTextColor;
+    private Drawable rightBackground;
+    private String rightText;
+
+    //标题的描述信息
+    private float titleTextSize;
+    private int titleTextColor;
+    private String title;
+
+    //放在layout中
+    private LayoutParams leftParams, rightParams, titleParams;
+
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public Topbar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        //自定义属性获取
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.Topbar);
+
+        leftTextColor = ta.getColor(R.styleable.Topbar_leftTextColor, 0);
+        leftBackground = ta.getDrawable(R.styleable.Topbar_leftBackground);
+        leftText = ta.getString(R.styleable.Topbar_leftText);
+
+        rightTextColor = ta.getColor(R.styleable.Topbar_rightTextColor, 0);
+        rightBackground = ta.getDrawable(R.styleable.Topbar_rightBackground);
+        rightText = ta.getString(R.styleable.Topbar_rightText);
+
+        titleTextSize = ta.getDimensionPixelSize(R.styleable.Topbar_titleTextSize, 0);
+        titleTextColor = ta.getColor(R.styleable.Topbar_titleTextColor, 0);
+        title = ta.getString(R.styleable.Topbar_titleText);
+
+        //回收ta
+        ta.recycle();
+
+        //实例化button和view
+        leftButton = new Button(context);
+        rightButton = new Button(context);
+        tvTitle = new TextView(context);
+
+        //获取到的属性赋值给控件
+        leftButton.setTextColor(leftTextColor);
+        leftButton.setBackground(leftBackground);
+        leftButton.setText(leftText);
+
+        //获取到的属性赋值给控件
+        rightButton.setTextColor(rightTextColor);
+        rightButton.setBackground(rightBackground);
+        rightButton.setText(rightText);
+
+        tvTitle.setTextColor(titleTextColor);
+        tvTitle.setTextSize(titleTextSize);
+        tvTitle.setText(title);
+        tvTitle.setGravity(Gravity.CENTER);//居中
+
+        setBackgroundColor(0xFFF59563);
+
+//        //放在layout中,传入长宽信息
+//        leftParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+//        //居左对其
+//        leftParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, TRUE);
+//        addView(leftButton, leftParams);
+
+        //放在layout中,传入长宽信息,下列设置宽高为WRAP_CONTENT
+        leftParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        //增加规则：居左对其
+        leftParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, TRUE);
+        addView(leftButton, leftParams);
+
+        //放在layout中,传入长宽信息,下列设置宽高为WRAP_CONTENT
+        rightParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        //增加规则：居左对其
+        rightParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, TRUE);
+        addView(rightButton, rightParams);
+
+        //放在layout中,传入长宽信息,下列设置宽高为WRAP_CONTENT
+        titleParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
+        //增加规则：居左对其
+        titleParams.addRule(RelativeLayout.CENTER_IN_PARENT, TRUE);
+        addView(tvTitle, titleParams);//通过addView加入相关视图
+    }
+}
