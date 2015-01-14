e36fb87132415aabc1937e645dfe00325993f397
tenanty <tenanty@gmail.com>
2015-01-14 22:57:54 +0800

    增加设置页面效果

---
+++ app/src/main/java/com/channelsoft/alps/activity/to/Settings.java
@@ -0,0 +1,11 @@
+package com.channelsoft.alps.activity.to;
+
+/**
+ * Created by tenanty on 15/1/14.
+ */
+public class Settings {
+    private String settingsName;//设置信息
+    private int settingsImg;//图片信息
+
+
+}
